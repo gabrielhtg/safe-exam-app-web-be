9efc839de9e2bfd62dc6cc497030bd145560d32f
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-10-18 16:17:40 +0700

    membuat api autentikasi login

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { UsersService } from '../users/users.service';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+    UsersModule,
+  ],
+})
+export class AuthModule {}
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: '4prDcUj3By5gfEPCaCZFpw9zih8WC5ibtCyZTzXu1c8mqi3fAX',
+};
