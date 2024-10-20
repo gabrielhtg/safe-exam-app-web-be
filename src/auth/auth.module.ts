a73bc9b9ed862810746784cc23a893385b3ad024
unknown <yuliandari.tiara@gmail.com>
2024-10-20 16:13:05 +0700

    api registration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule,ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+// import { jwtConstants } from './constants';
 import { UsersService } from '../users/users.service';
 import { UsersModule } from '../users/users.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
   imports: [
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
-    }),
+    ConfigModule.forRoot(),
     UsersModule,
+    JwtModule.registerAsync({
+      imports : [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRATION'),
+        }
+      }),
+    }),
   ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from 'src/model/auth.register.dto';
+import { PrismaService } from './prisma.service';
+import * as bcrypt from 'bcrypt';
+import { console } from 'inspector/promises';
 
 @Injectable()
 export class AuthService {
+  
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private prisma: PrismaService,
   ) {}
 
   async signIn(
@@ -20,6 +26,51 @@ export class AuthService {
     const payload = { sub: user.userId, username: user.username };
     return {
       access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async signUp( registerDto: RegisterDto){
+    const {
+      name,
+      username,
+      email,
+      password,
+      confirmPass,
+
+    } = registerDto
+
+    const existingUser = await this.prisma.user.findUnique({
+      where:{ email},
+    });
+
+    console.log("pass: ", registerDto.password);
+    console.log("confirmpass: ", registerDto.confirmPass);
+
+    if(existingUser){
+      throw new BadRequestException('Email already used');
+    }
+
+    if (password !== confirmPass){
+      throw new BadRequestException('Passwords do not match');
+    }
+
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+    const newUser = await this.prisma.user.create({
+      // ...registerDto,
+      // password: hashedPassword,
+      data: {
+        name,
+        username,
+        email,
+        password :hashedPassword,
+      },
+    });
+
+    const payload = {username: newUser.username, sub: newUser.id};
+    const token = this.jwtService.sign(payload);
+    return{
+      access_token:token,
     };
   }
 }
